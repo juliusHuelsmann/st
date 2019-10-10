33a91bd875229eff4b1a4a72b895e31a01d4c7fb
Julius Hülsmann <juliusHuelsmann@gmail.com>
2019-10-10 08:55:03 +0100

    [PATCH] External pipe (tiny adaptation)

--- st.h
+++ st.h
@@ -80,6 +80,7 @@ void die(const char *, ...);
 void redraw(void);
 void draw(void);
 
+void externalpipe(const Arg *);
 void kscrolldown(const Arg *);
 void kscrollup(const Arg *);
 void printscreen(const Arg *);
