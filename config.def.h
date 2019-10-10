2ac1a489d1e64cb1dbaaec656fd4bbc8a0997cef
Julius Hülsmann <juliusHuelsmann@gmail.com>
2019-10-10 08:51:19 +0100

    [PATH] scroll patch with altered key combi as default

--- config.def.h
+++ config.def.h
@@ -183,6 +183,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,          XK_k,     kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_j,   kscrolldown,    {.i = -1} },
 };
 
 /*
