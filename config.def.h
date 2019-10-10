3982c5a762d3a490ea7fdaba4e77b6fb4de0dbc9
Julius Hülsmann <juliusHuelsmann@gmail.com>
2019-10-10 10:10:24 +0100

    [CONFIG]: fixed the scroll command which seems to work only with TERMMOD when using capital letters.

--- config.def.h
+++ config.def.h
@@ -191,8 +191,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ControlMask,          XK_K,     kscrollup,      {.i = -1} },
-	{ ControlMask,          XK_J,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,          XK_K,     kscrollup,      {.i = -1} },
+	{ TERMMOD,          XK_J,   kscrolldown,    {.i = -1} },
 	{ ControlMask,          XK_k,     kscrollup,      {.i = 1} },
 	{ ControlMask,          XK_j,   kscrolldown,    {.i = 1} },
 };
