c1bfc85a7d02a49bf9b013486235c53b2dcf4a77
Julius Huelsmann <juliusHuelsmann@gmail.com>
2021-04-25 18:23:36 +0200

    change custom settings

--- config.def.h
+++ config.def.h
@@ -126,12 +126,12 @@ static const char *colorname[] = {
 	"black",
 };
 
-unsigned int const currentBg = 6, buffSize = 2048;
+unsigned int const currentBg = 8, buffSize = 2048;
 /// [Vim Browse] Colors for search results currently on screen.
 unsigned int const highlightBg = 160, highlightFg = 15;
 char const wDelS[] = "!\"#$%&'()*+,-./:;<=>?@[\\]^`{|}~", wDelL[] = " \t";
 char *nmKeys [] = {              ///< Shortcusts executed in normal mode
-  "R/Building\nN", "r/Building\n", "X/juli@machine\nN", "x/juli@machine\n",
+  "R/Building\nN", "r/Building\n", "X/x@machine\nN", "x/x@machine\n",
   "Q?[Leaving vim, starting execution]\n","F/: error:\nN", "f/: error:\n", "DQf"
 };
 unsigned int const amountNmKeys = sizeof(nmKeys) / sizeof(*nmKeys);
@@ -148,7 +148,7 @@ unsigned int defaultfg = 7;
 unsigned int defaultbg = 0;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-unsigned int bg = 17, bgUnfocused = 16;
+unsigned int bg = 16, bgUnfocused = 0;
 
 /*
  * Default shape of cursor
